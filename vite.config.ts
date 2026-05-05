c174c8a249e09c92cb91cf122a04d5af9b05c402
Gurdeep-Code <s.gurdeep9986@gmail.com>
2026-05-05 22:36:33 +0530

    Update configuration files to set 'admission-chat' as the output directory for builds in Vite, Netlify, and Vercel, and add it to .gitignore for cleaner project management.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   base: "/admission-chat/",
+  build: {
+    outDir: "admission-chat",
+  },
   server: {
     host: "::",
     port: 3000,
