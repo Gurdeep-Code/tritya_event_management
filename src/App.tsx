b4ba995af69c3f288e19789ad5457ffbbc8b96f4
Gurdeep-Code <s.gurdeep9986@gmail.com>
2026-05-06 00:18:21 +0530

    Add redirect from root path to '/admission-chat' in App.tsx for improved navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,6 +16,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/admission-chat" replace={true} />}/>
           <Route path="/admission-chat" element={<Outlet />}>
             <Route index element={<Index />} />
             <Route path="thank-you" element={<ThankYou />} />
